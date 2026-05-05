f0767c8b2f5ca8913c62668116bb51e59dc6ad8e
matsuokashuhei <matsuokashuheiii@gmail.com>
2026-05-05 03:36:18 +0000

    :fire: Remove seaography crate and related files

--- apps/graphql/src/main.rs
+++ apps/graphql/src/main.rs
@@ -1,26 +1,5 @@
-use async_graphql::{
-    dynamic::Schema,
-    http::{GraphQLPlaygroundConfig, playground_source},
-};
-use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
-use axum::{
-    Router,
-    extract::State,
-    response::{self, IntoResponse},
-    routing::get,
-};
-use seaography::async_graphql;
+use axum::{Router, http::StatusCode, routing::get};
 use tokio::net::TcpListener;
-
-async fn graphql_playground() -> impl IntoResponse {
-    let endpoint = "http://localhost:3000/graphql";
-    response::Html(playground_source(GraphQLPlaygroundConfig::new(endpoint)))
-}
-
-async fn graphql_handler(State(schema): State<Schema>, req: GraphQLRequest) -> GraphQLResponse {
-    let req = req.into_inner();
-    schema.execute(req).await.into()
-}
 
 #[tokio::main]
 async fn main() {
@@ -28,11 +7,7 @@ async fn main() {
         .with_max_level(tracing::Level::INFO)
         .with_test_writer()
         .init();
-    let schema = walking_dog::query_root::build_schema().await.unwrap();
-    let app = Router::new()
-        .route("/graphql", get(graphql_playground).post(graphql_handler))
-        .route("/health", get(|| async { "ok" }))
-        .with_state(schema);
+    let app = Router::new().route("/health", get(|| async { StatusCode::OK }));
     let addr = std::net::SocketAddr::from(([0, 0, 0, 0], 3000));
     axum::serve(TcpListener::bind(addr).await.unwrap(), app)
         .await
